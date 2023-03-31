116b6b9b5e02c0ef78936545ac0827648aaae00f
KaioRodrigoDev <kaiorodrigocoder@gmail.com>
2023-03-30 23:21:33 -0300

    feat(book): creating MainCard to books

---
+++ src/components/Cards/MainCard/index.tsx
@@ -0,0 +1,27 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {CardProps} from './interface';
+
+const MainCard = ({title, children}: CardProps) => {
+  return (
+    <View className="w-full h-24 bg-[#D7D7D7] rounded-md flex-row overflow-hidden">
+      <View className="w-24 m-1 bg-black" />
+      <View className="justify-center w-3/4 pl-2">
+        <Text
+          className="max-w-[90%] text-lg font-extrabold text-[#000]"
+          numberOfLines={1}>
+          {title}
+        </Text>
+        {children && (
+          <Text
+            className="max-w-[90%] mt-1 font-medium text-[#636363] "
+            numberOfLines={2}>
+            {children}
+          </Text>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default MainCard;
