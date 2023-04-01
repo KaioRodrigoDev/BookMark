30c02a6ea94e6e406827c135a11bd91875b893ce
KaioRodrigoDev <kaiorodrigocoder@gmail.com>
2023-04-01 02:41:14 -0300

    feat(book): creating Books page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,42 +1,122 @@
 import {View, TouchableOpacity, FlatList} from 'react-native';
-import React from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import MainCard from '@Components/Cards/MainCard';
-
+import {BookProps} from '@Types/home/books';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import ModalCreate from './components/modal';
+import NewBookModal from './components/BookModal';
+import {useNavigation} from '@react-navigation/native';
 
 const Home = () => {
-  const books = [
-    {
-      title: 'como fazer amigos e influenciar pessoas',
-      description:
-        'Book Descriptions DescriptionsDescriptionsDescriptions DescriptionsDescriptionsDesc riptionsDescriptio nsDescriptions',
+  const [books, setBooks] = useState<[] | BookProps[]>([]);
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [warningModal, setWarningModal] = useState(false);
+  const {navigate} = useNavigation();
+
+  useEffect(() => {
+    async function loadTask() {
+      const taskStorage = await AsyncStorage.getItem('@Books');
+
+      if (taskStorage) {
+        setBooks(JSON.parse(taskStorage));
+      }
+
+      console.log(taskStorage);
+    }
+
+    loadTask();
+  }, []);
+
+  useEffect(() => {
+    AsyncStorage.setItem('@Books', JSON.stringify(books));
+  }, [books]);
+
+  const handlePress = useCallback(
+    (item: string) => {
+      //@ts-ignore
+      navigate('Chapters', item);
     },
-    {
-      title: 'A Arte da guerra',
-      description:
-        'Book Desc DescriptionsDescriptions DescriptionsDescriptionsDesc riptionsDescriptio nsDescriptions',
+    [navigate],
+  );
+
+  const handleAdd = useCallback(
+    (title: string, description: string) => {
+      function hasBook(titleBook: string) {
+        return books.some(element => element.title === titleBook);
+      }
+      try {
+        const data = {
+          title: title,
+          description: description,
+        };
+        if (books.length === 0) {
+          setBooks([data]);
+          return data;
+        }
+
+        if (hasBook(title) === true) {
+          return setWarningModal(true);
+        }
+        console.log('title');
+
+        setBooks([...books, data]);
+      } catch (err) {
+        return err;
+      } finally {
+        setModalVisible(false);
+      }
     },
-    {
-      title: 'Relacionamentos importam',
-      description:
-        'Book Descriptions DescriptionsDescriptionsDescriptions DescriptionsDescriptionsDesc riptionsDescriptio nsDescriptions',
+    [books, setBooks],
+  );
+
+  function handleOpen() {
+    setModalVisible(true);
+  }
+
+  const handleDelete = useCallback(
+    (title: string) => {
+      const removed = books.filter(r => r.title !== title);
+      setBooks(removed);
     },
-  ];
+    [setBooks, books],
+  );
 
   return (
     <>
       <View>
         <View className="px-4 mt-4">
-          <FlatList
-            ItemSeparatorComponent={() => <View className="pb-4" />}
-            data={books}
-            renderItem={({item}) => (
-              <MainCard title={item.title}>{item.description}</MainCard>
-            )}
-          />
+          {books && (
+            <FlatList
+              ItemSeparatorComponent={() => <View className="pb-4" />}
+              data={books}
+              renderItem={({item}) => (
+                <MainCard
+                  title={item.title}
+                  handlePress={handlePress}
+                  handleDelete={handleDelete}>
+                  {item.description}
+                </MainCard>
+              )}
+            />
+          )}
         </View>
+
+        <ModalCreate
+          visible={warningModal}
+          onClose={() => setWarningModal(false)}
+        />
+
+        <NewBookModal
+          visible={modalVisible}
+          onSend={handleAdd}
+          onClose={() => setModalVisible(false)}
+        />
       </View>
-      <TouchableOpacity className="absolute items-center content-center justify-center px-5 py-4 bg-gray-900 rounded-full bottom-2 right-2">
+      <TouchableOpacity
+        onPress={() => handleOpen()}
+        className="absolute items-center content-center justify-center px-5 py-4 bg-gray-900 rounded-full bottom-2 right-2">
         <Icon name="plus" size={35} color="#ddd" />
       </TouchableOpacity>
     </>
