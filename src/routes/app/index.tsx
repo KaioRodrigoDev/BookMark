641f3b4f8427ad84c6b5de25b5644c091034d65d
KaioRodrigoDev <kaiorodrigocoder@gmail.com>
2023-04-01 02:43:28 -0300

    feat(chapters): creating chapters route

--- src/routes/app/index.tsx
+++ src/routes/app/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Home from '@Pages/Home';
+import Chapters from '@Pages/Chapters';
 
 const {Navigator, Screen} = createNativeStackNavigator();
 
@@ -11,10 +12,22 @@ const AuthRoutes = () => (
     screenOptions={{
       headerShown: false,
       contentStyle: {
-        backgroundColor: '#fff',
+        backgroundColor: '#403F4C',
       },
     }}>
     <Screen name="Home" component={Home} />
+    <Screen
+      name="Chapters"
+      //@ts-ignore
+      component={Chapters}
+      options={{
+        headerShown: true,
+        title: 'Capitulos',
+        headerTitle: 'Capitulos',
+        headerTitleAlign: 'center',
+        headerShadowVisible: false,
+      }}
+    />
   </Navigator>
 );
 
