fc765c16027d3f0cb6522a72de5e4dc2ee250ad3
KaioRodrigoDev <kaiorodrigocoder@gmail.com>
2023-04-01 02:42:51 -0300

    feat(type): creating type

---
+++ src/types/home/books/index.ts
@@ -0,0 +1,8 @@
+export interface BookProps {
+  title: string;
+  description: string;
+}
+
+export interface HasBookProps {
+  titulo: string;
+}
---
+++ src/types/storage/index.ts
@@ -0,0 +1,3 @@
+export interface GetStorageProps {
+  taskStorage: string | null;
+}
